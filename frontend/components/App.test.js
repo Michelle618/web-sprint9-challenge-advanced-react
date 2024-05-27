a20f9afcb27d6118a591a1486efdc006820adf01
Michelle Staton <mstaton618@gmail.com>
2024-05-27 12:56:19 -0400

    work in progress

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,55 +1,33 @@
-import React from 'react'
-import {render, waitFor, screen }from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom'
-import server from '../../backend/mock-server'
-import { resetAppFunctional } from '../../backend/helpers'
-import AppFunctional from './AppFunctional.js'
+import React from 'react';
+import AppFunctional from './AppFunctional';
+import { render, screen, fireEvent} from '@testing-library/react';
 
-// describe('AppFunctional Component', () => {
-  // let user, email, steps, message, index, x, y,
-  afterEach(() => {server.resetHandlers() })
-  beforeAll(() => {server.listen() }) 
-  afterAll(() => {server.close() })
-  beforeEach(async () => {
-    resetAppFunctional()
-    render(<appFunctional />)
-  let user = userEvent.setup()
-  })
-// })
+test('renders initial coordinates and steps', () => {
+  render(<AppFunctional />);
+  console.log('AppFunctional')
+  expect(screen.getByText("Coordinates(2, 2)")).toBeInTheDocument();
+  expect(screen.getByText("You moved 0 times")).toBeInTheDocument();
+});
 
-test('component renders', () => {
-  render(<AppFunctional component/>)
-  expect(screen.getByText('Welcome to the Grid')).toBeVisible
+test('moves correctly and updates coordinates and steps', () => {
+  render(<AppFunctional />);
+  
+  fireEvent.click(screen.getByText('LEFT'));
+  expect(screen.getByText("You moved 1 time")).toBeInTheDocument();
+  expect(screen.getByText(/Coordinates \(1, 2\)/)).toBeInTheDocument();
 
-})
+  fireEvent.click(screen.getByText('UP'));
+  expect(screen.getByText(/You moved 2 times/)).toBeInTheDocument();
+  expect(screen.getByText(/Coordinates \(1, 1\)/)).toBeInTheDocument();
 
-test('can type input and submit it', () => {async
-  expect(await screen.findByText('mstaton618 win #32')).toBeVisible
+  fireEvent.click(screen.getByText('RIGHT'));
+  expect(screen.getByText(/You moved 3 times/)).toBeInTheDocument();
+  expect(screen.getByText(/Coordinates \(2, 1\)/)).toBeInTheDocument();
 
-})
-
-test('submit email and get proper message', () => {async
-  expect(await screen.findByText('mstaton618 win #32')).toBeVisible
-}
-
-
-test('proper error message - 422 (forbidden email)', () => {async
-  expect(await screen.findByText('foo@bar.baz failure #32')).toBeVisible
-}
-
-
-test('coordinates rendering', () => {
-  expect(screen.getByText(`Coordinates${(x,y)}`)).toBeVisible
-
-})
-// Write your tests here
+  fireEvent.click(screen.getByText('DOWN'));
+  expect(screen.getByText(/You moved 4 times/)).toBeInTheDocument();
+  expect(screen.getByText(/Coordinates \(2, 2\)/)).toBeInTheDocument();
+});
 test('sanity', () => {
   expect(true).toBe(true)
 })
-
-// test('email placeholder is present', async => () => {
-// await waitFor(() => {
-//   screen.getByPlaceholderText('type email')
-// })
-// })
